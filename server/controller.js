4fc203d9d2072c22c49ec3eda76b038e91a7879d
haydenbetts <haydenbetts@gmail.com>
2018-11-13 21:45:21 -0800

    [progress] factored out query params handler

--- server/controller.js
+++ server/controller.js
@@ -3,48 +3,52 @@ const Comments = require('../database/models');
 
 const { Op } = Sequelize;
 
+const queryParamsHandler = (req, res) => {
+  const { id } = req.params;
+  const { type, limit, filters } = req.query;
+  let field;
+  if (type === 'relevant') {
+    field = 'user';
+  } else if (type === 'helpfulButton') {
+    field = 'yesRating';
+  } else if (type === 'newest') {
+    field = 'date';
+  }
+
+  if (filters !== '[]') {
+    console.log(filters);
+    Comments.findAll({
+      order: [[`${field}`, 'DESC']],
+      limit: parseInt(limit, 10),
+      where: { prodRating: { [Op.or]: JSON.parse(filters) }, prodId: id },
+    })
+      .then((result) => {
+        res.status(200).send(result);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  } else {
+    Comments.findAll({
+      order: [[`${field}`, 'DESC']],
+      limit: parseInt(limit, 10),
+      where: { prodId: id },
+    })
+      .then((result) => {
+        res.status(200).send(result);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }
+}
+
 module.exports = {
   get: (req, res) => {
-    const { id } = req.params;
-
     if (Object.keys(req.query).length > 0) {
-      const { type, limit, filters } = req.query;
-      let field;
-      if (type === 'relevant') {
-        field = 'user';
-      } else if (type === 'helpfulButton') {
-        field = 'yesRating';
-      } else if (type === 'newest') {
-        field = 'date';
-      }
-
-      if (filters !== '[]') {
-        console.log(filters);
-        Comments.findAll({
-          order: [[`${field}`, 'DESC']],
-          limit: parseInt(limit, 10),
-          where: { prodRating: { [Op.or]: JSON.parse(filters) }, prodId: id },
-        })
-          .then((result) => {
-            res.status(200).send(result);
-          })
-          .catch((err) => {
-            console.error(err);
-          });
-      } else {
-        Comments.findAll({
-          order: [[`${field}`, 'DESC']],
-          limit: parseInt(limit, 10),
-          where: { prodId: id },
-        })
-          .then((result) => {
-            res.status(200).send(result);
-          })
-          .catch((err) => {
-            console.error(err);
-          });
-      }
+      queryParamsHandler(req, res);
     } else {
+      const { id } = req.params;
       Comments.findAll({ where: { prodId: id } })
         .then((result) => {
           res.status(200).send(result);
