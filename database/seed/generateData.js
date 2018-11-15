451c8fa04371127739fa10386f65f76899d420f1
haydenbetts <haydenbetts@gmail.com>
2018-11-15 14:00:41 -0800

    updated generateData file

---
+++ database/seed/generateData.js
@@ -0,0 +1,35 @@
+const faker = require('faker');
+const fs = require('fs');
+const dataToSeed = fs.createWriteStream('./database/dataToSeed.csv');
+
+//dataToSeed.write('user, prodRating, yesRating, noRating, date, body, verified, recommend, size, width, comfort, quality, response, prodId, header\n');
+
+console.time("generateData");
+
+for (let j = 1; j <= 1000; j++) {
+  for (let i = 0; i < 10000; i++) {
+    const response = Math.random() > 0.5 ? ' ' : faker.lorem.sentence();
+    const header = Math.random() > 0.5 ? ' ' : faker.lorem.sentence();
+    dataToSeed.write(
+      "" + faker.internet.userName() + ","
+      + faker.random.number({ min: 1, max: 5 }) + ","
+      + faker.random.number({ min: 0, max: 200 }) + ","
+      + faker.random.number({ min: 0, max: 200 }) + ","
+      + faker.date.past().toISOString() + ","
+      + faker.lorem.sentences() + ","
+      + faker.random.boolean() + ","
+      + faker.random.boolean() + ","
+      + faker.random.number({ min: 1, max: 4 }) + ","
+      + faker.random.number({ min: 1, max: 4 }) + ","
+      + faker.random.number({ min: 1, max: 4 }) + ","
+      + faker.random.number({ min: 1, max: 4 }) + ","
+      + response + ","
+      + j + ","
+      + header
+      + "\n"
+    )
+  }
+}
+
+dataToSeed.end();
+console.timeEnd("generateData");
